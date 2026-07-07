958478d5bc88c6e54ec186d3e2a42bfc9bc8ac87
Riteshkumar2e <riteshkumar90359@gmail.com>
2026-07-07 17:51:37 +0530

    changed app.py

--- api/app.py
+++ api/app.py
@@ -73,4 +73,4 @@ if _dashboard.exists():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("api.app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("api.app:app", host="0.0.0.0", port=8000, reload=True)
